f06e6756612490c744ef7fc476cb4108a637f064
taiwoomotoye99-dev <taiwoomotoye99@gmail.com>
2026-08-25 15:05:40 +0100

    Create loader.py

---
+++ data/loader.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from data.config import settings
+
+bot = Bot(
+    token=settings.BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+dp = Dispatcher()
